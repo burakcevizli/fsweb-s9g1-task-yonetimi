7710ba0c68a937d486c2a17c055b38e43163cb66
KrieqqeR <burakcevizli@gmail.com>
2023-08-21 19:15:09 +0300

    Ödev güncel

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,7 +1,65 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
-export default function TaskHookForm() {
+export default function TaskHookForm({kisiler , submitFn}) {
+  const { register, handleSubmit, formState: { errors } } = useForm({mode:"all"});
+  const onSubmit = data => submitFn(data);
+  
+  console.log(errors)
+  
   return (
-    <div>Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm dosyasındaki HTML yapısını vs app.css içerisindeki classları kullanabilirsiniz.</div>
-  )
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="title">Başlığınız :</label>
+      <input
+        type="text"
+        id="title"
+        placeholder="Title"
+        {...register("title", {
+          required: "Başlık alanı boş bırakılamaz !",
+          minLength: {
+            value: 3,
+            message: "Başlık en az 3 karakter içermelidir",
+          }
+        })}
+      />
+      {errors.title && <p>{errors.title.message}</p>}
+      
+      <label htmlFor="description">Açıklama :</label>
+      <input
+        type="text"
+        id="description"
+        placeholder="Açıklama"
+        {...register("description", {
+          required: "Mutlaka Açıklama Yazmalısınız!",
+          minLength: {
+            value: 10,
+            message: "Açıklama en az 10 karakter içermelidir",
+          }
+        })}
+      />
+      {errors.description && <p>{errors.description.message}</p>}
+
+      <label>İnsanlar :</label>
+      {kisiler.map((herbirkisi, i) => (
+        <div key={i}>
+          <input
+            type="checkbox"
+            {...register('people', {
+              required: 'En az bir kişi seçmelisiniz !',
+            })}
+          />
+          {herbirkisi}
+        </div>
+      ))}
+      {errors.people && <p>{errors.people.message}</p>}
+          
+      
+      
+      <input type="submit"  value="Görevi Ekle"/>
+
+        
+
+      
+    </form>
+  );
 }
